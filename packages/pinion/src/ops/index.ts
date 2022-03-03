18c40237a0d7310ded11a55b62af4994a9de57fe
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-03-03 21:25:25 +0100

    feat: init op inject

--- packages/pinion/src/ops/index.ts
+++ packages/pinion/src/ops/index.ts
@@ -1,3 +1,4 @@
 export * from './prompt'
 export * from './render'
+export * from './inject'
 export * from './run'
---
+++ packages/pinion/src/ops/inject.ts
@@ -0,0 +1,95 @@
+import { existsSync, write } from 'fs'
+import { readFile, writeFile } from "fs/promises"
+import { PinionContext, Callable, getCallable, Promisable } from '../core'
+import { EOL } from 'os'
+
+const EOLRegex = /\r?\n/
+
+export type Location<C extends PinionContext> = (lines: string[], ctx: any) => Promisable<{ index: number, pattern: string | RegExp }>
+
+export const inject = <C extends PinionContext> (template: Callable<string, C>, location: Location<C>, target: Callable<string, C>) =>
+  async <T extends C> (ctx: T) => {
+    const fileName = await getCallable(target, ctx)
+    const content = await getCallable(template, ctx)
+
+    const { force, logger, prompt } = ctx.pinion
+
+    if (!existsSync(fileName)) {
+      throw new Error(`Cannot inect to '${fileName}'. The file doesn't exist.`);
+    }
+
+    const oldContent = (await readFile(fileName)).toString()
+
+    const NL = newline(content)
+    const lines = content.split(NL)
+
+    const { pattern , index } = await location(lines, ctx);
+
+    if (index >= 0) {
+      lines.splice(index, 0, content)
+    }
+
+    const newContent = lines.join(NL)
+
+    await writeFile(fileName, newContent);
+  }
+
+const getLineNumber = (
+  pattern: string | RegExp,
+  lines: string[],
+  isBefore: boolean,
+) => {
+  const oneLineMatchIndex = lines.findIndex((l) => l.match(pattern))
+
+  if (oneLineMatchIndex < 0) {
+    const fullText = lines.join('\n')
+    const fullMatch = fullText.match(new RegExp(pattern, 'm'))
+
+    if (fullMatch && fullMatch.length) {
+      if (isBefore) {
+        const fullTextUntilMatchStart = fullText.substring(0, fullMatch.index)
+        return fullTextUntilMatchStart.split(EOLRegex).length - 1
+      }
+      const matchEndIndex = (fullMatch.index || 0) + fullMatch.toString().length
+      const fullTextUntilMatchEnd = fullText.substring(0, matchEndIndex)
+      return fullTextUntilMatchEnd.split(EOLRegex).length
+    }
+  }
+
+  return oneLineMatchIndex + (isBefore ? 0 : 1)
+}
+
+export const before = <C extends PinionContext> (pattern: Callable<string | RegExp, C>) => async (lines: string[], ctx: any) => {
+  const line = await getCallable(pattern, ctx);
+  const index = getLineNumber(line, lines, true);
+  return { index, pattern: line }
+}
+
+export const after = <C extends PinionContext> (pattern: Callable<string |  RegExp, C>) => async (lines: string[], ctx: any) => {
+  const line = await getCallable(pattern, ctx);
+  const index = getLineNumber(line, lines, false);
+  return { index, pattern: line }
+}
+
+export const prepend = <C extends PinionContext> (pattern: Callable<string |  RegExp, C>) => async (lines: string[], ctx: any) => {
+  const line = await getCallable(pattern, ctx);
+  return { index: 0, pattern: line }
+}
+
+export const append = <C extends PinionContext> (pattern: Callable<string |  RegExp, C>) => async (lines: string[], ctx: any) => {
+  const line = await getCallable(pattern, ctx);
+  return { index: lines.length - 1, pattern: line }
+}
+
+const newline = (str: string) => {
+  const newlines = str.match(/(?:\r?\n)/g) || []
+
+  if (newlines.length === 0) {
+      return EOL
+  }
+
+  const crlf = newlines.filter((newline) => newline === '\r\n').length
+  const lf = newlines.length - crlf
+
+  return crlf > lf ? '\r\n' : '\n'
+}
